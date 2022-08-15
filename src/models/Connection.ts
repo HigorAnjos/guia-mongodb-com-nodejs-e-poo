0f170d67319ec218927c88e703b87ae43e8236f6
Higor Anjos <higorc.anjos@gmail.com>
2022-08-15 15:46:13 -0300

    fim da aula

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
+// mongodb://root:example@localhost:27017/glassesStore?authSource=admin
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin',
+    || 'mongodb://localhost:27017/?readPreference=primary&ssl=false',
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
